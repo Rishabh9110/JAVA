1b50c78650302716478ed9516aa6446451cd5a35
Rishabh91110 <rrishabh283@gmail.com>
2026-06-07 17:18:37 +0530

    Initial commit

---
+++ Challengers/src/Main.java
@@ -0,0 +1,12 @@
+// print all pyramid star pattern using single print statement
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("*\n* *\n* * *\n* * * *\n* * * * *");
+        System.out.println();
+        System.out.println("* * * * *\n* * * *\n* * *\n* *\n*");
+        System.out.println();
+        System.out.println("        *\n      * *\n    * * *\n  * * * *\n* * * * *");
+
+    }
+}
