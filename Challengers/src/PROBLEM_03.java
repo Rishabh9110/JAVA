5e9ce3ea04dc9e5c4b39ec689ab962c804bc242c
Rishabh91110 <rrishabh283@gmail.com>
2026-06-07 21:34:18 +0530

    updating code

---
+++ Challengers/src/PROBLEM_03.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class PROBLEM_03 {
+
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter the first number:");
+        int num1=input.nextInt();
+        System.out.print("Enter the Second number:");
+        int num2=input.nextInt();
+        System.out.println("SUM OF BOTH NUMBERS ARE:"+(num1+num2));
+
+    }
+}
